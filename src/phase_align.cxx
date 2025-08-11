5668f00716abdf7d08a32b4b3d8cff76487d6357
Roy Ratcliffe <roy@ratcliffe.me>
2025-08-11 08:15:11 +0100

    Add detailed documentation and licensing information to blt.hxx, blt.cxx, and phase_align.cxx

--- src/phase_align.cxx
+++ src/phase_align.cxx
@@ -1,3 +1,25 @@
+/// SPDX-License-Identifier: MIT
+///
+/// \copyright 2025, Roy Ratcliffe, Northumberland, United Kingdom
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+/// documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+/// rights to use, copy, modify, merge, publish, distribute, sub-license, and/or sell copies of the Software, and to
+/// permit persons to whom the Software is furnished to do so, subject to the following conditions:
+///
+///     The above copyright notice and this permission notice shall be included in all copies or substantial
+///     portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+/// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+/// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+/// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+///
+/// \file phase_align.cxx
+/// \brief Phase alignment for bit-plane operations.
+/// \details This file contains the implementation of the PhaseAlign class and its derived classes,
+///          which handle the fetching and storing of bits with respect to their phase alignment.
+
 #include "phase_align.hxx"
 
 // Exchange and shift --- some in-line assembler macros.  GNU has a
