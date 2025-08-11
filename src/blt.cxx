f080cd80cc32f87a2313959c1a2657e5929cfddd
Roy Ratcliffe <roy@ratcliffe.me>
2025-08-11 07:34:10 +0100

    Update operand definitions and comments for clarity on bit operations

--- src/blt.cxx
+++ src/blt.cxx
@@ -1,7 +1,7 @@
 #include "blt.hxx"
 
-#define D (*store) // 32-bit destination operand
-#define S fetch()  // 32-bit source operand
+#define D (*store) // 8-bit destination operand
+#define S fetch()  // 8-bit source operand
 
 #define ropRevPolish(revPolish, x)                                                                                     \
   scanbyte Blt::rop##revPolish() { return x; }
@@ -12,8 +12,8 @@ ropRevPolish(0, 0x00U) ropRevPolish(DSon, ~(D | S)) ropRevPolish(DSna, D & ~S) r
             ropRevPolish(S, S) ropRevPolish(SDno, S | ~D) ropRevPolish(DSo, D | S) ropRevPolish(1, 0xffU)
 
     // A BitPlane::Rop2 code translates to one of 16 rop() functions.  The
-    // rop() functions execute their Boolean operation simultaneously on 32
-    // bits, returning the 32-bit answer.  Static Blt-member vrop is an
+    // rop() functions execute their Boolean operation simultaneously on 16
+    // bits, returning the 8-bit answer.  Static Blt-member vrop is an
     // array of pointers to the 16 Blt-class member functions implementing
     // the 16 raster operations.
     Blt::Rop Blt::vrop[] = {
