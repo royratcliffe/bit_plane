7b7eb7fb616d11992ac693e32fc473c692a5d65b
Roy Ratcliffe <roy@ratcliffe.me>
2025-08-11 08:09:20 +0100

    Update copyright and licensing information in BitPlane header and source files; add brief file descriptions.

--- inc/bit_plane.hxx
+++ inc/bit_plane.hxx
@@ -1,27 +1,28 @@
-//**    $Id: BitPlane.hh,v 1.4 2002/03/22 16:17:31 royr Exp $
-//
-//      Copyright (C) 1996, 1998, 1999, 2002, Roy Ratcliffe, United Kingdom.
-//      All rights reserved.
-//
-//      Permission is hereby granted, free of charge,  to any person obtaining a
-//      copy  of  this  software  and    associated   documentation  files  (the
-//      "Software"), to deal in  the   Software  without  restriction, including
-//      without limitation the rights to  use,   copy,  modify,  merge, publish,
-//      distribute, sublicense, and/or sell  copies  of   the  Software,  and to
-//      permit persons to whom the Software is   furnished  to do so, subject to
-//      the following conditions:
-//
-//          The above copyright notice and this permission notice shall be
-//          included in all copies or substantial portions of the Software.
-//
-//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT  WARRANTY OF ANY KIND, EXPRESS
-//      OR  IMPLIED,  INCLUDING  BUT  NOT   LIMITED    TO   THE   WARRANTIES  OF
-//      MERCHANTABILITY, FITNESS FOR A PARTICULAR   PURPOSE AND NONINFRINGEMENT.
-//      IN NO EVENT SHALL THE AUTHORS  OR   COPYRIGHT  HOLDERS BE LIABLE FOR ANY
-//      CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER   IN  AN ACTION OF CONTRACT,
-//      TORT OR OTHERWISE, ARISING FROM,  OUT  OF   OR  IN  CONNECTION  WITH THE
-//      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
+/// SPDX-License-Identifier: MIT
+///
+/// \copyright 1996, 1998, 1999, 2002, 2025, Roy Ratcliffe, Northumberland, United Kingdom
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+/// documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+/// rights to use, copy, modify, merge, publish, distribute, sub-license, and/or sell copies of the Software, and to
+/// permit persons to whom the Software is furnished to do so, subject to the following conditions:
+///
+///     The above copyright notice and this permission notice shall be included in all copies or substantial
+///     portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+/// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+/// OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+/// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+///
+/// \file bit_plane.hxx
+/// \brief BitPlane class definition and operations.
+/// \details This file contains the definition of the BitPlane class, which represents a rectangular array of bits
+///          mapped one-to-one with pixels. The class provides operations for creating, manipulating, and
+///          destroying bit planes.
+
+#pragma once
+
 //**    Name
 //
 //      BitPlane --- rectangular arrays of bits
@@ -121,11 +122,11 @@ public:
   int getHeight() const { return height; }
 
 protected:
-  int width = 0; ///< Width of the bit-plane.
-  int height = 0; ///< Height of the bit-plane.
-  int widthScanBytes = 0; ///< Width of the bit-plane in scan bytes.
+  int width = 0;             ///< Width of the bit-plane.
+  int height = 0;            ///< Height of the bit-plane.
+  int widthScanBytes = 0;    ///< Width of the bit-plane in scan bytes.
   scanbyte *store = nullptr; ///< Pointer to the bit-plane scan bytes.
-  bool autoDelete = false; ///< Flag indicating whether to delete the bit-plane.
+  bool autoDelete = false;   ///< Flag indicating whether to delete the bit-plane.
 
   /// \brief Find the scan byte containing the bit at the specified coordinates.
   /// \param x X-coordinate of the bit.
--- src/bit_plane.cxx
+++ src/bit_plane.cxx
@@ -1,27 +1,32 @@
-//**    $Id: BitPlane.cc,v 1.3 2002/03/19 12:19:40 royr Exp $
-//
-//      Copyright (C) 1996, 1998, 1999, Roy Ratcliffe, United Kingdom.
-//      All rights reserved.
-//
-//      Permission is hereby granted, free of charge,  to any person obtaining a
-//      copy  of  this  software  and    associated   documentation  files  (the
-//      "Software"), to deal in  the   Software  without  restriction, including
-//      without limitation the rights to  use,   copy,  modify,  merge, publish,
-//      distribute, sublicense, and/or sell  copies  of   the  Software,  and to
-//      permit persons to whom the Software is   furnished  to do so, subject to
-//      the following conditions:
-//
-//          The above copyright notice and this permission notice shall be
-//          included in all copies or substantial portions of the Software.
-//
-//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT  WARRANTY OF ANY KIND, EXPRESS
-//      OR  IMPLIED,  INCLUDING  BUT  NOT   LIMITED    TO   THE   WARRANTIES  OF
-//      MERCHANTABILITY, FITNESS FOR A PARTICULAR   PURPOSE AND NONINFRINGEMENT.
-//      IN NO EVENT SHALL THE AUTHORS  OR   COPYRIGHT  HOLDERS BE LIABLE FOR ANY
-//      CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER   IN  AN ACTION OF CONTRACT,
-//      TORT OR OTHERWISE, ARISING FROM,  OUT  OF   OR  IN  CONNECTION  WITH THE
-//      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
+/// SPDX-License-Identifier: MIT
+///
+/// \copyright 1996, 1998, 1999, 2002, 2025, Roy Ratcliffe, Northumberland, United Kingdom
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+/// documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+/// rights to use, copy, modify, merge, publish, distribute, sub-license, and/or sell copies of the Software, and to
+/// permit persons to whom the Software is furnished to do so, subject to the following conditions:
+///
+///     The above copyright notice and this permission notice shall be included in all copies or substantial
+///     portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+/// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+/// OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+/// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+///
+/// \file bit_plane.cxx
+/// \brief BitPlane class implementation.
+/// \details This file contains the implementation of the BitPlane class, which represents a rectangular array of bits
+///          mapped one-to-one with pixels. The class provides operations for creating, manipulating, and
+///          destroying bit planes.
+
+#include "bit_plane.hxx"
+#include "blt.hxx"
+
+#include <cassert> // for assert()
+#include <cstring> // for memcpy()
+
 //**    Name
 //
 //      BitPlane --- rectangular arrays of bits
@@ -77,12 +82,6 @@
 //              }
 //
 //**********************************************************************
-
-#include "bit_plane.hxx"
-#include "blt.hxx"
-
-#include <cassert> // for assert()
-#include <cstring> // for memcpy()
 
 BitPlane::BitPlane() : width(0), height(0), store(0) {
   // Setting width and height to zero makes the BitPlane valid and
